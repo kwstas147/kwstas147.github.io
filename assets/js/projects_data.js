9079ace6b1420a469f6c3cd82dc394a0a90a4098
Kwstas <kwstas147@gmail.com>
2026-07-04 23:22:14 +0300

    feat: redesign programming page with dynamic project details view

---
+++ assets/js/projects_data.js
@@ -0,0 +1,174 @@
+const projectsData = {
+    "easyslot": {
+        "id": "easyslot",
+        "titleKey": "pages.programming.projects.easySlot.title",
+        "descKey": "pages.programming.projects.easySlot.description",
+        "icon": "assets/images/development/easyslot/icon.png",
+        "tags": [
+            "Flutter",
+            "Firebase",
+            "Dart",
+            "SaaS",
+            "QR Codes"
+        ],
+        "downloadType": "apk",
+        "downloadAvailable": false,
+        "downloadLink": "",
+        "downloadFilename": "",
+        "screenshots": [
+            "assets/images/development/easyslot/optimized_assets/eas",
+            "assets/images/development/easyslot/optimized_assets/eas1",
+            "assets/images/development/easyslot/optimized_assets/eas2",
+            "assets/images/development/easyslot/optimized_assets/eas3",
+            "assets/images/development/easyslot/optimized_assets/eas4",
+            "assets/images/development/easyslot/optimized_assets/eas5",
+            "assets/images/development/easyslot/optimized_assets/eas6"
+        ]
+    },
+    "myplan": {
+        "id": "myplan",
+        "titleKey": "pages.programming.projects.myPlan.title",
+        "descKey": "pages.programming.projects.myPlan.description",
+        "icon": "assets/images/development/myplan/icon.png",
+        "tags": [
+            "Flutter",
+            "Dart",
+            "Health Connect",
+            "Mobile App"
+        ],
+        "downloadType": "apk",
+        "downloadAvailable": true,
+        "downloadLink": "assets/downloads/myplan/app-arm64-v8a-release.apk",
+        "downloadFilename": "MyPlan.apk",
+        "screenshots": [
+            "assets/images/development/myplan/optimized_assets/mp",
+            "assets/images/development/myplan/optimized_assets/mp1",
+            "assets/images/development/myplan/optimized_assets/mp2",
+            "assets/images/development/myplan/optimized_assets/mp3"
+        ]
+    },
+    "synapses": {
+        "id": "synapses",
+        "titleKey": "pages.programming.projects.synapses.title",
+        "descKey": "pages.programming.projects.synapses.description",
+        "icon": "assets/images/development/synapses/icon.png",
+        "tags": [
+            "Flutter",
+            "Dart",
+            "BLE",
+            "IoT",
+            "Mobile App"
+        ],
+        "downloadType": "apk",
+        "downloadAvailable": true,
+        "downloadLink": "assets/downloads/synapses/app-arm64-v8a-release.apk",
+        "downloadFilename": "Synapses.apk",
+        "screenshots": [
+            "assets/images/development/synapses/optimized_assets/syn",
+            "assets/images/development/synapses/optimized_assets/syn1",
+            "assets/images/development/synapses/optimized_assets/syn2",
+            "assets/images/development/synapses/optimized_assets/syn3",
+            "assets/images/development/synapses/optimized_assets/syn4"
+        ]
+    },
+    "synthesis": {
+        "id": "synthesis",
+        "titleKey": "pages.programming.projects.synthesis.title",
+        "descKey": "pages.programming.projects.synthesis.description",
+        "icon": "assets/images/development/synthesis/icon.png",
+        "tags": [
+            "Flutter",
+            "Dart",
+            "AI",
+            "Laboratory",
+            "Gemini"
+        ],
+        "downloadType": "apk",
+        "downloadAvailable": false,
+        "downloadLink": "",
+        "downloadFilename": "",
+        "screenshots": [
+            "assets/images/development/synthesis/optimized_assets/synt",
+            "assets/images/development/synthesis/optimized_assets/synt1",
+            "assets/images/development/synthesis/optimized_assets/synt2",
+            "assets/images/development/synthesis/optimized_assets/synt3",
+            "assets/images/development/synthesis/optimized_assets/synt4",
+            "assets/images/development/synthesis/optimized_assets/synt5",
+            "assets/images/development/synthesis/optimized_assets/synt6",
+            "assets/images/development/synthesis/optimized_assets/synt7",
+            "assets/images/development/synthesis/optimized_assets/synt8",
+            "assets/images/development/synthesis/optimized_assets/synt9"
+        ]
+    },
+    "duplicatefinder": {
+        "id": "duplicatefinder",
+        "titleKey": "pages.programming.projects.duplicateFinder.title",
+        "descKey": "pages.programming.projects.duplicateFinder.description",
+        "icon": "assets/images/development/DuplicateFinder/icon.ico",
+        "tags": [
+            "Python",
+            "PySide6",
+            "Desktop App",
+            "Optimization",
+            "SHA-256"
+        ],
+        "downloadType": "installer",
+        "downloadAvailable": false,
+        "downloadLink": "",
+        "downloadFilename": "",
+        "screenshots": []
+    },
+    "myinventory": {
+        "id": "myinventory",
+        "titleKey": "pages.programming.projects.myInventory.title",
+        "descKey": "pages.programming.projects.myInventory.description",
+        "icon": "assets/images/development/myinventory/icon.png",
+        "tags": [
+            "Flutter",
+            "Dart",
+            "Mobile App",
+            "Privacy First"
+        ],
+        "downloadType": "apk",
+        "downloadAvailable": true,
+        "downloadLink": "assets/downloads/myinventory/app-arm64-v8a-release.apk",
+        "downloadFilename": "MyInventory.apk",
+        "screenshots": [
+            "assets/images/development/myinventory/optimized_assets/inv",
+            "assets/images/development/myinventory/optimized_assets/inv1",
+            "assets/images/development/myinventory/optimized_assets/inv2",
+            "assets/images/development/myinventory/optimized_assets/inv3",
+            "assets/images/development/myinventory/optimized_assets/inv4",
+            "assets/images/development/myinventory/optimized_assets/inv5",
+            "assets/images/development/myinventory/optimized_assets/inv6",
+            "assets/images/development/myinventory/optimized_assets/inv7",
+            "assets/images/development/myinventory/optimized_assets/inv8",
+            "assets/images/development/myinventory/optimized_assets/inv9"
+        ]
+    },
+    "fueltracker": {
+        "id": "fueltracker",
+        "titleKey": "pages.programming.projects.fuelTracker.title",
+        "descKey": "pages.programming.projects.fuelTracker.description",
+        "icon": "assets/images/development/Fuel Operation Tracker/icon.png",
+        "tags": [
+            "Flutter",
+            "Dart",
+            "Cloud",
+            "Mobile App"
+        ],
+        "downloadType": "apk",
+        "downloadAvailable": false,
+        "downloadLink": "",
+        "downloadFilename": "",
+        "screenshots": [
+            "assets/images/development/Fuel Operation Tracker/converted/1",
+            "assets/images/development/Fuel Operation Tracker/converted/2",
+            "assets/images/development/Fuel Operation Tracker/converted/3",
+            "assets/images/development/Fuel Operation Tracker/converted/4",
+            "assets/images/development/Fuel Operation Tracker/converted/5",
+            "assets/images/development/Fuel Operation Tracker/converted/6",
+            "assets/images/development/Fuel Operation Tracker/converted/7"
+        ]
+    }
+};
